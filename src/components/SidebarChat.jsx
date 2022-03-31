caeb7c931142b4e454955418cbfb66dfec1c981e
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2022-03-31 10:35:54 -0300

    final

--- src/components/SidebarChat.jsx
+++ src/components/SidebarChat.jsx
@@ -5,6 +5,19 @@ import { Link } from 'react-router-dom'
 
 function SidebarChat({ addNewChat, name, id }) {
     const [seed, setSeed] = useState('')
+    const [messages, setMessages] = useState('')
+
+    useEffect(() => {
+      if(id) {
+        db.collection('rooms')
+         .doc(id)
+         .collection('messages')
+         .orderBy('timestamp', 'desc')
+         .onSnapshot(snapshot => {
+           setMessages(snapshot.docs.map((doc) => doc.data()))
+         })
+      }
+    },[id])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
@@ -25,7 +38,7 @@ function SidebarChat({ addNewChat, name, id }) {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="sidebar-chat-info">
             <h2>{name}</h2>
-            <p>Last message..</p>
+            <p>{messages[0]?.message}</p>
         </div>
     </div>
     </Link>
